86f6c1ac1f4278aedaa5a0a12f922699436da620
Vitaly Alpatov <exclude@bk.ru>
2023-10-25 19:43:39 +0300

    ~ Рефакторинг модуля температуры

--- firmware/stm/source/temp.cpp
+++ firmware/stm/source/temp.cpp
@@ -5,64 +5,10 @@
 #include "xmath.h"
 #include "system.h"
 
-// Alias
-#define DMA1_C4     DMA1_Channel4
-#define DMA1_C5     DMA1_Channel5
-
-// Аппаратный дополнительный делитель частоты UART
-#define TEMP_UART_DIV_K                 16.0
-// Вещественный делитель UART
-#define TEMP_UART_DIV(baud)             (FMCU_NORMAL_HZ / ((float64_t)(baud)) / TEMP_UART_DIV_K)
-// Целая часть делителя UART
-#define TEMP_UART_DIV_INT(baud)         ((uint32_t)TEMP_UART_DIV(baud))
-// Дробная часть делителя UART
-#define TEMP_UART_DIV_FRAC(baud)        (TEMP_UART_DIV(baud) - TEMP_UART_DIV_INT(baud))
-// Конвертирование дробной части делителя UART в целую
-#define TEMP_UART_DIV_FRAC_I(baud)      ((uint32_t)(TEMP_UART_DIV_FRAC(baud) * TEMP_UART_DIV_K + 0.5))
-// Подсчет регистра BRR по скорости
-#define TEMP_UART_BRR_BY_BAUD(baud)     ((TEMP_UART_DIV_INT(baud) << 4) | (TEMP_UART_DIV_FRAC_I(baud) & 0x0F))
-
-// Размер буфера DMA в байтах
-#define TEMP_DMA_BUFFER_SIZE            32
-// Получает, активно ли DMA
-#define TEMP_DMA_IS_ACTIVE              (DMA1_C5->CCR & DMA_CCR_EN)
-
-// Код определения присутствия ведомого
-#define TEMP_DETECT_PRESENCE_CODE       0xF0
-
-// Конверитрование бита в байт
-#define TEMP_BIT_TO_BYTE(byte, bit)     ((byte) & (1 << (bit)) ? 0xFF : 0x00)
-// Генерация байтового массива по байту
-#define TEMP_BYTE_ARRAY_GEN(byte)       \
-    TEMP_BIT_TO_BYTE(byte, 0),          \
-    TEMP_BIT_TO_BYTE(byte, 1),          \
-    TEMP_BIT_TO_BYTE(byte, 2),          \
-    TEMP_BIT_TO_BYTE(byte, 3),          \
-    TEMP_BIT_TO_BYTE(byte, 4),          \
-    TEMP_BIT_TO_BYTE(byte, 5),          \
-    TEMP_BIT_TO_BYTE(byte, 6),          \
-    TEMP_BIT_TO_BYTE(byte, 7)
-
-// Запрос к чипу для старта измерения
-static const uint8_t TEMP_MEASURE_REQUEST_START[] =
-{
-    // Пропуск ROM (одно устройство на шине)
-    TEMP_BYTE_ARRAY_GEN(0xCC),
-    // Измерение
-    TEMP_BYTE_ARRAY_GEN(0x44),
-};
-
-// Запрос к чипу для чтения результатов измерения
-static const uint8_t TEMP_MEASURE_REQUEST_READ[] =
-{
-    // Пропуск ROM (одно устройство на шине)
-    TEMP_BYTE_ARRAY_GEN(0xCC),
-    // Чтение карты памяти
-    TEMP_BYTE_ARRAY_GEN(0xBE),
-    // Чтение двух байт
-    TEMP_BYTE_ARRAY_GEN(0xFF),
-    TEMP_BYTE_ARRAY_GEN(0xFF),
-};
+// Используемый канал DMA TX
+static DMA_Channel_TypeDef * const TEMP_DMA_C4 = DMA1_Channel4;
+// Используемый канал DMA RX
+static DMA_Channel_TypeDef * const TEMP_DMA_C5 = DMA1_Channel5;
 
 // Текущее состояние автомата
 static __no_init enum
@@ -79,11 +25,17 @@ static __no_init enum
     TEMP_STATE_MEASURE_READING_FIN,
 } temp_state;
 
+
 // Буферы DMA
 static __no_init struct
 {
-    uint8_t tx[TEMP_DMA_BUFFER_SIZE];
-    uint8_t rx[TEMP_DMA_BUFFER_SIZE];
+    // Прототип буфера DMA
+    typedef uint8_t buffer_t[88];
+    
+    // Передача
+    buffer_t tx;
+    // Приём
+    buffer_t rx;
 } temp_dma;
 
 // Состояние сброса чипа
@@ -123,19 +75,26 @@ static void temp_dma_channel_init(DMA_Channel_TypeDef *channel, const void *mem,
         USART1->DR, 
         // В память
         mem);
+    
     channel->CCR |= DMA_CCR_MINC | flags;                                       // Direction (flags), Memory increment (8-bit), Peripheral size 8-bit
 }
+
+// Получает, не активно ли DMA (для assert)
+#define TEMP_DMA_IS_INACTIVE      ((TEMP_DMA_C5->CCR & DMA_CCR_EN) == 0)
 
 // Запуск DMA для датчика температуры
 static void temp_dma_start(uint8_t size)
 {
     // Проверка аргументов и состояний
     assert(size > 0);
-    assert(!TEMP_DMA_IS_ACTIVE);
+    assert(size <= sizeof(temp_dma.tx));
+    assert(TEMP_DMA_IS_INACTIVE);
+    
     // Запуск DMA
-    DMA1_C4->CNDTR = DMA1_C5->CNDTR = size;                                     // Transfer data size
-    DMA1_C5->CCR |= DMA_CCR_EN;                                                 // Channel enable
-    DMA1_C4->CCR |= DMA_CCR_EN;                                                 // Channel enable
+    TEMP_DMA_C4->CNDTR = TEMP_DMA_C5->CNDTR = size;                             // Transfer data size
+    TEMP_DMA_C5->CCR |= DMA_CCR_EN;                                             // Channel enable
+    TEMP_DMA_C4->CCR |= DMA_CCR_EN;                                             // Channel enable
+    
     // Запуск таймаута
     temp_timeout_timer_restart();
 }
@@ -145,14 +104,15 @@ static void temp_dma_stop(void)
 {
     // Стоп таймера таймаута
     temp_timeout_timer.stop();
+    
     // Стоп DMA
     DMA1->IFCR |= DMA_IFCR_CTCIF5;                                              // Clear CTCIF
-    DMA1_C4->CCR &= ~DMA_CCR_EN;                                                // Channel disable
-    DMA1_C5->CCR &= ~DMA_CCR_EN;                                                // Channel disable
+    TEMP_DMA_C4->CCR &= ~DMA_CCR_EN;                                            // Channel disable
+    TEMP_DMA_C5->CCR &= ~DMA_CCR_EN;                                            // Channel disable
 }
 
 // Сброс текущего значения
-static void temp_current_set_reset(void)
+static void temp_current_reset(void)
 {
     temp_current = 99.9f;
 }
@@ -165,17 +125,42 @@ static void temp_chip_error(void)
     // Переход в начальное состояние
     temp_idle_state();
     // Сброс покзаания
-    temp_current_set_reset();
+    temp_current_reset();
 }
+
+// Подсчет регистра BRR по скорости
+constexpr uint32_t temp_uart_brr_by_baud(uint32_t baud)
+{
+    // Аппаратный дополнительный делитель частоты UART
+    const auto DIV_K = 16.0;
+    
+    // Вещественный делитель UART
+    const auto DIV = FMCU_NORMAL_HZ / ((float64_t)baud) / DIV_K;
+    // Целая часть делителя UART
+    const auto DIV_I = (uint32_t)DIV;
+    
+    // Дробная часть делителя UART
+    const auto DIV_FRAC = DIV - DIV_I;
+    // Конвертирование дробной части делителя UART в целую
+    const auto DIV_FRAC_I = (uint32_t)(DIV_FRAC * DIV_K + 0.5);
+
+    // Конечный подсчет регистра BRR
+    return (DIV_I << 4) | (DIV_FRAC_I & 0x0F);
+}
+
+// Код определения присутствия ведомого
+constexpr const uint8_t TEMP_DETECT_PRESENCE_CODE = 0xF0;
 
 // Запуск процедуры сброса чипа
 static void temp_chip_reset(void)
 {
-    assert(!TEMP_DMA_IS_ACTIVE);
+    assert(TEMP_DMA_IS_INACTIVE);
+    
     // Состояние сброса
     temp_reseting = true;
     // Переход на низкую скорость
-    USART1->BRR = TEMP_UART_BRR_BY_BAUD(9600);                                  // Baudrate 9600
+    USART1->BRR = temp_uart_brr_by_baud(9600);                                  // Baudrate 9600
+    
     // Подготовка данных к передаче и отправка
     temp_dma.tx[0] = TEMP_DETECT_PRESENCE_CODE;
     temp_dma_start(1);
@@ -205,29 +190,50 @@ static void temp_timeout_timer_cb(void)
     }
 }
 
-// Линеаризация текущей температуры
-static void temp_current_linearization(void)
+// Конверитрование бита в байт
+constexpr uint8_t temp_bit_to_byte(uint8_t byte, uint8_t bit)
 {
-    // Точка для линеаризации (взято из графика в даташите)
-    static const math_point2d_t<float_t, float_t> POINTS[] =
-    {
-        { 0.0f,     0.15f },
-        { 10.0f,    0.18f },
-        { 20.0f,    0.20f },
-        { 30.0f,    0.18f },
-        { 40.0f,    0.14f },
-        { 50.0f,    0.07f },
-        { 60.0f,    -0.01f },
-        { 70.0f,    -0.14f },
-    };
-    
-    temp_current += math_linear_interpolation(temp_current, POINTS, array_length(POINTS));
+    return (byte & (1 << bit)) ? 0xFF : 0x00;
 }
+
+// Расчет контрольной суммы дныых датчика
+uint8_t temp_dallas_crc(const void *data)
+{
+	uint8_t crc = 0;
+    const uint8_t *u8 = (const uint8_t *)data;
+	
+	for (auto i = 0; i < 8; i++)
+    {
+		auto temp = *u8++;
+		for (auto j = 0; j < 8; j++, temp >>= 1)
+        {
+			const auto mix = (crc ^ temp) & 0x01;
+			crc >>= 1;
+			
+            if (mix) 
+                crc ^= 0x8C;
+		}
+	}
+    
+	return crc;
+}
+
+// Генерация байтового массива по байту
+#define TEMP_BYTE_ARRAY_GEN(byte)       \
+    temp_bit_to_byte(byte, 0),          \
+    temp_bit_to_byte(byte, 1),          \
+    temp_bit_to_byte(byte, 2),          \
+    temp_bit_to_byte(byte, 3),          \
+    temp_bit_to_byte(byte, 4),          \
+    temp_bit_to_byte(byte, 5),          \
+    temp_bit_to_byte(byte, 6),          \
+    temp_bit_to_byte(byte, 7)
 
 // Обработчик события завершения передачи по DMA
 static void temp_dma_event_cb(void)
 {
     assert(temp_state != TEMP_STATE_IDLE);
+    
     // Если сброс
     if (temp_reseting)
     {
@@ -239,7 +245,7 @@ static void temp_dma_event_cb(void)
             return;
         }
         // Переход на высокую скорость
-        USART1->BRR = TEMP_UART_BRR_BY_BAUD(115200);                            // Baudrate 115200
+        USART1->BRR = temp_uart_brr_by_baud(115200);                            // Baudrate 115200
     }
     
     // Далее по состояниям
@@ -251,8 +257,20 @@ static void temp_dma_event_cb(void)
             
         case TEMP_STATE_MEASURE_START:
             // Запрос
-            memcpy(temp_dma.tx, TEMP_MEASURE_REQUEST_START, sizeof(TEMP_MEASURE_REQUEST_START));
-            temp_dma_start(sizeof(TEMP_MEASURE_REQUEST_START));
+            {
+                // Запрос к чипу для старта измерения
+                static const uint8_t REQUEST[] =
+                {
+                    // Пропуск ROM (одно устройство на шине)
+                    TEMP_BYTE_ARRAY_GEN(0xCC),
+                    // Измерение
+                    TEMP_BYTE_ARRAY_GEN(0x44),
+                };
+                
+                memcpy(temp_dma.tx, REQUEST, sizeof(REQUEST));
+                temp_dma_start(sizeof(REQUEST));
+            }
+            
             // Далее ожидание результатов измерения
             temp_state = TEMP_STATE_MEASURE_WAIT;
             break;
@@ -267,35 +285,122 @@ static void temp_dma_event_cb(void)
         case TEMP_STATE_MEASURE_READING:
             // Запрос
             {
-                memcpy(temp_dma.tx, TEMP_MEASURE_REQUEST_READ, sizeof(TEMP_MEASURE_REQUEST_READ));
-                temp_dma_start(sizeof(TEMP_MEASURE_REQUEST_READ));
+                // Запрос к чипу для чтения результатов измерения
+                static const uint8_t REQUEST[] =
+                {
+                    // Пропуск ROM (одно устройство на шине)
+                    TEMP_BYTE_ARRAY_GEN(0xCC),
+                    // Чтение карты памяти
+                    TEMP_BYTE_ARRAY_GEN(0xBE),
+                    // Чтение 9 байт
+                    TEMP_BYTE_ARRAY_GEN(0xFF), TEMP_BYTE_ARRAY_GEN(0xFF),
+                    TEMP_BYTE_ARRAY_GEN(0xFF), TEMP_BYTE_ARRAY_GEN(0xFF),
+                    TEMP_BYTE_ARRAY_GEN(0xFF), TEMP_BYTE_ARRAY_GEN(0xFF),
+                    TEMP_BYTE_ARRAY_GEN(0xFF), TEMP_BYTE_ARRAY_GEN(0xFF),
+                    TEMP_BYTE_ARRAY_GEN(0xFF),
+                };
+                
+                memcpy(temp_dma.tx, REQUEST, sizeof(REQUEST));
+                temp_dma_start(sizeof(REQUEST));
             }
+            
             // Далее завершение чтения результатов
             temp_state = TEMP_STATE_MEASURE_READING_FIN;
             break;
             
         case TEMP_STATE_MEASURE_READING_FIN:
-            temp_current = 0;
-            for (auto i = 16; i < 27; i++)
             {
-                // Только установленные биты
-                if (temp_dma.rx[i] != 0xFF)
-                    continue;
-                // Степень
-                auto base = i - 16 - 4;
-                auto neg = base < 0;
-                if (neg)
-                    base = -base;
-                // Возвещение в степень
-                auto pow = 1 << base;
-                // Инкремент разряда
-                temp_current += neg ? 1 / pow : pow;
+                // Индексы полей структуры данных датчика
+                enum
+                {
+                    // Младшая часть значения
+                    POS_LSB,
+                    // Старшая часть значения
+                    POS_MSB,
+                    // Верхний аварийный порог
+                    POS_TH,
+                    // Нижний аварийный порог
+                    POS_TL,
+                    // Конфигурация
+                    POS_CFG,
+                    // Первое магическое значение
+                    POS_MAGIC_1,
+                    // Резерв
+                    POS_RESERVED,
+                    // Второе магическое значение
+                    POS_MAGIC_2,
+                    // Контрольная сумма
+                    POS_CRC
+                };
+                
+                union
+                {
+                    // Сырое значение
+                    uint8_t u8;
+                    // Старшая часть значения
+                    struct
+                    {
+                        uint8_t value : 3;
+                        bool sign : 1;
+                    } msb;
+                } raw[9] = { 0 };
+                
+                // Сброка по битам
+                for (auto i = 0; i < sizeof(raw); i++)
+                {
+                    const auto base = i * 8 + 16;
+                    for (auto j = base; j < base + 8; j++)
+                    {
+                        raw[i].u8 >>= 1;
+                        if (temp_dma.rx[j] == 0xFF)
+                            raw[i].u8 |= 0x80;
+                    }
+                    
+                }
+                
+                // Проверка контрольной суммы
+                if (raw[POS_MAGIC_1].u8 != 0xFF || 
+                    raw[POS_MAGIC_2].u8 != 0x10 || 
+                    temp_dallas_crc(raw) != raw[POS_CRC].u8)
+                {
+                    // Ошибка и в начальное состояние
+                    temp_current_reset();
+                    temp_idle_state();
+                    break;
+                }
+                
+                // Целое значение температуры
+                uint16_t u16 = raw[POS_MSB].msb.value;
+                u16 <<= 8;
+                u16 |= raw[POS_LSB].u8;
+
+                // Вещественное значение температуры
+                temp_current = u16;
+                temp_current /= 16.0;
+
+                // Учет знака
+                if (raw[POS_MSB].msb.sign)
+                    temp_current = -temp_current;
             }
-            // Учет знака
-            if (temp_dma.rx[28] == 0xFF)
-                temp_current = -temp_current;
+            
             // Линеаризация
-            temp_current_linearization();
+            {
+                // Точка для линеаризации (взято из графика в даташите)
+                static const math_point2d_t<float_t, float_t> POINTS[] =
+                {
+                    { 0.0f,     0.15f },
+                    { 10.0f,    0.18f },
+                    { 20.0f,    0.20f },
+                    { 30.0f,    0.18f },
+                    { 40.0f,    0.14f },
+                    { 50.0f,    0.07f },
+                    { 60.0f,    -0.01f },
+                    { 70.0f,    -0.14f },
+                };
+                
+                temp_current += math_linear_interpolation(temp_current, POINTS, array_length(POINTS));
+            }
+            
             // В начальное состояние
             temp_idle_state();
             break;
@@ -323,12 +428,12 @@ void temp_init(void)
     // Включаем прерывание канала DMA
     nvic_irq_enable(DMA1_Channel5_IRQn);
     // Канал 4 (TX)
-    temp_dma_channel_init(DMA1_C4, temp_dma.tx, DMA_CCR_DIR);                   // Memory to peripheral
+    temp_dma_channel_init(TEMP_DMA_C4, temp_dma.tx, DMA_CCR_DIR);               // Memory to peripheral
     // Канал 5 (RX)
-    temp_dma_channel_init(DMA1_C5, temp_dma.rx, DMA_CCR_TCIE);                  // Transfer complete IRQ enable
+    temp_dma_channel_init(TEMP_DMA_C5, temp_dma.rx, DMA_CCR_TCIE);              // Transfer complete IRQ enable
     
     // Начальное состояние
-    temp_current_set_reset();
+    temp_current_reset();
     // Форсирование первого измерения
     temp_idle_state(TIMER_US_MIN);
 }
